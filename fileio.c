03546da27101ca42c463b29ddef30d240bcc2bfb
wallacegibbon <wallacegibbon@aliyun.com>
2025-08-21 15:33:26 +0800

    Bug Fix: The `ffgetline` did not copy the right/full data to `fline` buffer.
    The fix provided by f76962d595c5b0401c244cfcd3ce2f65d6578ee9 was wrong.
    It goes wrong when there is 0 ('\0') in the content.

--- fileio.c
+++ fileio.c
@@ -83,7 +83,7 @@ int ffgetline(int *count)
 		if (i >= flen) {
 			if ((tmpline = malloc(flen + NSTRING)) == NULL)
 				return FIOMEM;
-			strncpy(tmpline, fline, flen);
+			memcpy(tmpline, fline, flen);
 			flen += NSTRING;
 			free(fline);
 			fline = tmpline;
